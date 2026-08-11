13f3f187e52efb4e214030d02800731d09bc425c
frankinavarro1998-byte <franki.navarro1998@gmail.com>
2026-08-10 18:11:33 -0700

    Update __init__.py

--- app/retailers/__init__.py
+++ app/retailers/__init__.py
@@ -1,1 +1,12 @@
-# retailers
+
+from .generic import GenericChecker
+from .target import TargetChecker
+from .walmart import WalmartChecker
+from .topps import ToppsChecker
+
+CHECKERS = {
+    "target": TargetChecker(),
+    "walmart": WalmartChecker(),
+    "topps": ToppsChecker(),
+    "other": GenericChecker(),
+}
