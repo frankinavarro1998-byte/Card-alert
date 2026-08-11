1aab6f8ee55d2818064f4c5d391da074aca9632a
frankinavarro1998-byte <franki.navarro1998@gmail.com>
2026-08-10 18:57:16 -0700

    Refactor positive and negative signals in TargetChecker

--- app/retailers/target.py
+++ app/retailers/target.py
@@ -3,5 +3,16 @@ from .generic import GenericChecker
 
 class TargetChecker(GenericChecker):
     name = "target"
-    positive_signals = GenericChecker.positive_signals + ("shipping", "pick it up", "add for shipping")
-    negative_signals = GenericChecker.negative_signals + ("not eligible for shipping", "this item isn't available")
+
+    positive_signals = (
+        "add to cart",
+        "add for shipping",
+    )
+
+    negative_signals = GenericChecker.negative_signals + (
+        "not eligible for shipping",
+        "this item isn't available",
+        "item unavailable",
+        "out of stock",
+        "sold out",
+    )
